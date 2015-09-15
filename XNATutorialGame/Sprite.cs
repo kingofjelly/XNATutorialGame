62b86f7f15dbf18b851c82098d55fe7a4d51fcd9
Jelly King <curriedmc@gmail.com>
2015-09-15 20:02:43 +0100

    Initial committal of files
    Initial commit of files for my XNA game

---
+++ XNATutorialGame/Sprite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace XNATutorialGame
+{
+   
+
+    class Sprite
+    {
+        //aking sprite class
+
+        public Vector2 Position = new Vector2(0, 0);//position for this class objects. Accessible externally.
+
+        private Texture2D mSpriteTexture;//texture for this class. Can only be accessed within this class
+        //now need to load + draw + update content?
+
+    }
+}
