bc2674a1ff050a979212078f6fa4c3fd13254bb5
Jelly King <curriedmc@gmail.com>
2015-09-26 20:43:20 +0100

    Addition of array
    I've added a class for falling objects. I've then created and added them
    into an array, which generates them at random points. Beginnings of
    replication of AJ's cow game

---
+++ XNATutorialGame/FallingObjects.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+
+namespace XNATutorialGame
+{
+    class FallingObjects : Sprite //it inherits sprite
+    {
+        ///class for falling objects. For replication of something akin to cows calling game.
+        ///AssetName
+        ///Position x,y
+        ///Rectangle?
+        ///Speed
+        ///Velocity?
+        ///
+        const string WIZARD_ASSETNAME = "SquareGuy";//temp placeholder
+        const int START_POSITION_X = 125;
+        const int START_POSITION_Y = 245;
+        const int START_POSITION_X1 = 225;
+        const int START_POSITION_Y1 = 245;
+        const int START_POSITION_X2 = 325;
+        const int START_POSITION_Y2 = 245;
+        const int WIZARD_SPEED = 160;
+        const int MOVE_UP = -1;
+        const int MOVE_DOWN = 1;
+        const int MaximumTimeLeft = 500;
+
+        GameWideMethods GWM = new GameWideMethods();
+
+        Vector2 mDirection = Vector2.Zero;
+        Vector2 mSpeed = Vector2.Zero;
+        
+
+        public void LoadContent(ContentManager theContentManager)
+        {
+            //positions could be decided by RNG. Could also decide in here if fallingObArray < 4 in size, crete, else don't this run
+            int positionChoice = GWM.randomNumer();
+
+            if (positionChoice == 1)
+            {
+                Position = new Vector2(START_POSITION_X, START_POSITION_Y);
+                base.LoadContent(theContentManager, WIZARD_ASSETNAME);
+            }
+            if (positionChoice == 2)
+            {
+                Position = new Vector2(START_POSITION_X1, START_POSITION_Y1);
+                base.LoadContent(theContentManager, WIZARD_ASSETNAME);
+            }
+            if (positionChoice == 3)
+            {
+                Position = new Vector2(START_POSITION_X2, START_POSITION_Y2);
+                base.LoadContent(theContentManager, WIZARD_ASSETNAME);
+            }
+
+            //Position = new Vector2(START_POSITION_X, START_POSITION_Y);
+            //base.LoadContent(theContentManager, WIZARD_ASSETNAME);
+            //we use the LoadContent method in the base class. This is sprite
+        }
+        //base class also has draw, update
+
+        //Draw
+
+
+        //Update
+        public void Update(GameTime theGameTime)
+        {
+            KeyboardState aCurrentKeyboardState = Keyboard.GetState();            
+            UpdateMovement();
+            base.Update(theGameTime, mSpeed, mDirection);
+        }
+
+        //UpdatePosition
+
+        private void UpdateMovement()//updates movement. needed for positon and drawing
+        {
+
+            mSpeed = Vector2.Zero;
+            mDirection = Vector2.Zero;
+            //here, opposed to if key is pressed, update movemnt every sec
+            mSpeed.Y = WIZARD_SPEED;
+            mDirection.Y = MOVE_DOWN;
+
+           
+        }
+       
+    }
+}
--- XNATutorialGame/Game1.cs
+++ XNATutorialGame/Game1.cs
@@ -25,6 +25,8 @@ namespace XNATutorialGame
         Texture2D mSpriteTexture;//Texture2D is a 2d image
          */
         Wizard mWizardSprite;//gives Wizard an alias?
+        FallingObjects mFallingObjectSprite;
+        FallingObjects[] fallingObjectsArray = new FallingObjects[3];
 
 
         public Game1()
@@ -43,7 +45,11 @@ namespace XNATutorialGame
         {
             // TODO: Add your initialization logic here
             mWizardSprite = new Wizard();
-
+            mFallingObjectSprite = new FallingObjects();
+            for (int i = 0; i < fallingObjectsArray.Length; i++)
+            {
+                fallingObjectsArray[i] = new FallingObjects();
+            }
 
             base.Initialize();
         }
@@ -61,6 +67,11 @@ namespace XNATutorialGame
             //mSpriteTexture = this.Content.Load<Texture2D>("SquareGuy");
 
             mWizardSprite.LoadContent(this.Content);
+            mFallingObjectSprite.LoadContent(this.Content);
+            for (int i = 0; i < fallingObjectsArray.Length; i++)
+            {
+                fallingObjectsArray[i].LoadContent(this.Content);
+            }
         }
 
         /// <summary>
@@ -86,7 +97,11 @@ namespace XNATutorialGame
             // TODO: Add your update logic here
 
             mWizardSprite.Update(gameTime);
-
+            mFallingObjectSprite.Update(gameTime);
+            for (int i = 0; i < fallingObjectsArray.Length; i++)
+            {
+                fallingObjectsArray[i].Update(gameTime);
+            }
             base.Update(gameTime);
         }
 
@@ -102,7 +117,12 @@ namespace XNATutorialGame
             //Spritebatch object is auto created when windows game is made. this is what's used to draw 2d objects to screen. This needs to be begun and ended.
             spriteBatch.Begin();
             //spriteBatch.Draw(mSpriteTexture, mPosition, Color.White);
-            mWizardSprite.Draw(this.spriteBatch);
+            //mWizardSprite.Draw(this.spriteBatch);
+            mFallingObjectSprite.Draw(this.spriteBatch);
+            for (int i = 0; i < fallingObjectsArray.Length; i++)
+            {
+                fallingObjectsArray[i].Draw(this.spriteBatch);
+            }
             spriteBatch.End();
 
 
